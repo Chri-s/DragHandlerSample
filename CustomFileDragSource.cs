e0c99d8a1d2adebde508c886117c26a3cec777c8
Chri-s <doooozer@gmx.net>
2023-01-31 15:52:05 +0100

    Initial commit

---
+++ CustomFileDragSource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using GongSolutions.Wpf.DragDrop;
+
+namespace DragHandlerSample
+{
+    public class CustomFileDragSource : DefaultDragHandler
+    {
+        public override bool CanStartDrag(IDragInfo dragInfo)
+        {
+            // Check that the control which started the drag is a label and the content is a string.
+            // Can be changed to the desired control type
+            return (dragInfo.VisualSource is Label label && label.Content is string);
+        }
+
+        public override void StartDrag(IDragInfo dragInfo)
+        {
+            // Get the control that started the drag and the filename from the label
+            Label sourceControl = dragInfo.VisualSource as Label;
+            if (sourceControl == null || !(sourceControl.Content is string fileName))
+                return;
+
+            // Get the path of TextFile.txt in the output directory
+            string folder = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string path = Path.Combine(folder, fileName);
+
+            // Drag the TextFile.txt in the output directory
+            dragInfo.DataFormat = DataFormats.GetDataFormat(DataFormats.FileDrop);
+            dragInfo.Data = new string[] { path };
+            dragInfo.Effects = DragDropEffects.Copy;
+        }
+    }
+}
---
+++ CustomTextDragSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using GongSolutions.Wpf.DragDrop;
+
+namespace DragHandlerSample
+{
+    public class CustomTextDragSource : DefaultDragHandler
+    {
+        public override bool CanStartDrag(IDragInfo dragInfo)
+        {
+            // Check that the control which started the drag is a label.
+            // Can be changed to the desired control type
+            return (dragInfo.VisualSource is Label);
+        }
+
+        public override void StartDrag(IDragInfo dragInfo)
+        {
+            // Get the control that started the drag
+            Label sourceControl = dragInfo.VisualSource as Label;
+            if (sourceControl == null)
+                return;
+
+            // Drag the content of the label as text
+            dragInfo.DataFormat = DataFormats.GetDataFormat(DataFormats.Text);
+            dragInfo.Data = sourceControl.Content;
+            dragInfo.Effects = DragDropEffects.Copy;
+        }
+    }
+}
